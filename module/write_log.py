70ac82fbdd0e45dd75e5681a0d43030e47215678
NANNADA4 <coco96062233@icloud.com>
2024-10-14 17:10:29 +0900

    log file update

--- module/write_log.py
+++ module/write_log.py
@@ -6,4 +6,4 @@
 def write_log(log_path, file_path):
     """변환에 실패한 파일을 로그로 남깁니다"""
     with open(log_path, 'a', encoding='UTF-8') as file:
-        file.write(file_path)
+        file.write(f"{file_path}\n")
