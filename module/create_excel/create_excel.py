af9a3b3fd5869cd96a1f471b69adacbcebc59b9e
NANNADA4 <coco96062233@icloud.com>
2024-10-30 17:41:30 +0900

    update create excel

--- module/create_excel/create_excel.py
+++ module/create_excel/create_excel.py
@@ -4,16 +4,17 @@
 
 import os
 from natsort import natsorted
-from openpyxl import Workbook, load_workbook
 
 
 from module.create_excel.find_name import extract_cmt, extract_org
 from module.create_excel.load_excel import load_excel
+from module.extract_bookmark import extract_bookmark
 
 
 def create_excel(input_path, excel_path):
     """엑셀 파일을 생성합니다"""
-    ws = load_excel(excel_path).active
+    wb = load_excel(excel_path)
+    ws = wb.active
 
     book_id = input("BOOK_ID 시작번호를 입력해주세요 (오름차순으로 매겨집니다) : ")
 
@@ -21,3 +22,22 @@ def create_excel(input_path, excel_path):
         for file in natsorted(files):
             cmt = extract_cmt(file)
             org = extract_org(file)
+            last_row = ws.max_row
+            tmp = 1
+
+            for item in extract_bookmark(input_path):
+                last_row += tmp
+                if len(item) > 1 and item['level'] == 3:
+                    ws.cell(row=last_row, column=1, value=org)
+                    ws.cell(row=last_row, column=2, value=cmt)
+                    ws.cell(row=last_row, column=3,
+                            value=item['parent']['title'])
+                    ws.cell(row=last_row, column=4, value=book_id)
+                    ws.cell(row=last_row, column=5, value=item['SeqNo'])
+                    ws.cell(row=last_row, column=6, value=item['title'])
+                    ws.cell(row=last_row, column=9, value=file)
+                    ws.cell(row=last_row, column=10,
+                            value=os.path.join(root, file))
+                    tmp += 1
+        book_id += 1
+    wb.save(excel_path)
--- module/create_excel/process_create_excel.py
+++ module/create_excel/process_create_excel.py
@@ -1,10 +1,9 @@
 """BOOK_ID, SEQNO가 매겨진 엑셀파일을 생성합니다"""
 
 
-from module.tmp.tmp import tmp
+from module.create_excel.create_excel import create_excel
 
 
 def process_create(input_path, excel_path):
     """BOOK_ID, SEQNO가 매겨진 엑셀파일을 생성합니다"""
-    #! TODO => Remove tmp()
-    tmp(input_path, excel_path)
+    create_excel(input_path, excel_path)
