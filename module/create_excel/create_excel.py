453a8f34bd59e347a7c75ac14b39da89679ff265
NANNADA4 <coco96062233@icloud.com>
2024-10-31 10:17:12 +0900

    script 1 done

--- module/create_excel/create_excel.py
+++ module/create_excel/create_excel.py
@@ -16,28 +16,28 @@ def create_excel(input_path, excel_path):
     wb = load_excel(excel_path)
     ws = wb.active
 
-    book_id = input("BOOK_ID 시작번호를 입력해주세요 (오름차순으로 매겨집니다) : ")
+    book_id = int(input("BOOK_ID 시작번호를 입력해주세요 (오름차순으로 매겨집니다) : "))
 
     for root, _, files in os.walk(input_path):
         for file in natsorted(files):
             cmt = extract_cmt(file)
             org = extract_org(file)
-            last_row = ws.max_row
-            tmp = 1
 
-            for item in extract_bookmark(input_path):
-                last_row += tmp
+            tmp = 1
+            last_row = ws.max_row
+
+            for item in extract_bookmark(os.path.join(root, file)):
                 if len(item) > 1 and item['level'] == 3:
-                    ws.cell(row=last_row, column=1, value=org)
-                    ws.cell(row=last_row, column=2, value=cmt)
-                    ws.cell(row=last_row, column=3,
+                    ws.cell(row=last_row + tmp, column=1, value=org)
+                    ws.cell(row=last_row + tmp, column=2, value=cmt)
+                    ws.cell(row=last_row + tmp, column=3,
                             value=item['parent']['title'])
-                    ws.cell(row=last_row, column=4, value=book_id)
-                    ws.cell(row=last_row, column=5, value=item['SeqNo'])
-                    ws.cell(row=last_row, column=6, value=item['title'])
-                    ws.cell(row=last_row, column=9, value=file)
-                    ws.cell(row=last_row, column=10,
-                            value=os.path.join(root, file))
+                    ws.cell(row=last_row + tmp, column=4, value=book_id)
+                    ws.cell(row=last_row + tmp,
+                            column=5, value=item['SeqNo'])
+                    ws.cell(row=last_row + tmp,
+                            column=6, value=item['title'])
+                    ws.cell(row=last_row + tmp, column=9, value=file)
                     tmp += 1
-        book_id += 1
+            book_id += 1
     wb.save(excel_path)
