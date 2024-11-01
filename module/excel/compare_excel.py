b154a99b2f86f537425783ed3d79baeb7f2599d9
NANNADA4 <coco96062233@icloud.com>
2024-11-01 09:58:20 +0900

    script 2 Done

--- module/excel/compare_excel.py
+++ module/excel/compare_excel.py
@@ -33,16 +33,17 @@ def compare_excel(excel_1=Workbook, excel_2=Workbook) -> Workbook:  # pylint: di
             if len(attach_list) == 0:
                 continue
 
-        # 위원회, 피감기관, 위원명, BOOKID, SEQNO, 질의, 별첨리스트
+        # 위원회, 피감기관, 위원명, BOOKID, SEQNO, 질의, 파일명, 별첨리스트
         if len(attach_list) > 0:
             attach_excel1_info_list.append([ws1.cell(row=ws1_row_num, column=1).value,
                                             ws1.cell(row=ws1_row_num, column=2).value, ws1.cell(
                                             row=ws1_row_num, column=3).value,
                                             ws1.cell(row=ws1_row_num, column=4).value, ws1.cell(
                                             row=ws1_row_num, column=5).value,
-                                            ws1.cell(row=ws1_row_num, column=6).value, attach_list])
+                                            ws1.cell(row=ws1_row_num, column=6).value, ws1.cell(
+                                                row=ws1_row_num, column=9).value, attach_list])
 
-        # 행 번호, 리스트 dic. 엑셀 삽입시 역순으로
+        # 행 번호, 리스트 dic. 엑셀 삽입시 역순으로. 순서대로 삽입시 행번호 꼬임
             total_dic.update(
                 {ws1_row_num + 1: attach_excel1_info_list})
 
@@ -50,15 +51,22 @@ def compare_excel(excel_1=Workbook, excel_2=Workbook) -> Workbook:  # pylint: di
     sorted_keys = sorted(total_dic.keys(), reverse=True)
     final_dict = OrderedDict((key, total_dic[key]) for key in sorted_keys)
 
+    # dictionary 디버깅용
+    # with open('./log/data.txt', 'w', encoding='UTF-8') as file:
+    #     for key, value in final_dict.items():
+    #         file.write(f"{key}: {value}\n")
+
     for key, value in final_dict.items():
-        ws1.insert_rows(key, len(value[0][6]))
-        for cnt in range(len(value[0][6])):
+        ws1.insert_rows(key, len(value[0][7]))
+        for cnt in range(len(value[0][7])):
             ws1.cell(row=key + cnt, column=1, value=value[0][0])
             ws1.cell(row=key + cnt, column=2, value=value[0][1])
             ws1.cell(row=key + cnt, column=3, value=value[0][2])
             ws1.cell(row=key + cnt, column=4, value=value[0][3])
             ws1.cell(row=key + cnt, column=5, value=value[0][4])
             ws1.cell(row=key + cnt, column=6, value=value[0][5])
-            ws1.cell(row=key + cnt, column=7, value=value[0][6][cnt])
+            ws1.cell(row=key + cnt, column=9, value=value[0][6])
+            ws1.cell(row=key + cnt, column=7, value=value[0][7][cnt])
+        ws1.delete_rows(key - 1)
 
     return excel_1
